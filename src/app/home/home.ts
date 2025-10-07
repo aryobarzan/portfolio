21572315e631ef9781cf2db5008a7ccfc78b94cd
aryobarzan <aryobarzan.atashpendar@gmail.com>
2025-10-07 18:55:44 +0200

    replace href with routerlink fragments

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
 import { BioHeader } from '../bio-header/bio-header';
 import { Projects } from "../projects/projects";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [BioHeader, Projects],
+  imports: [BioHeader, Projects, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
   isActive(path: string): boolean {
-    console.log(window.location);
     return window.location.hash === path;
   }
 }
