c61348ec32deb7ac3d10fcd760d8511990aeec8d
aryobarzan <aryobarzan.atashpendar@gmail.com>
2026-05-18 20:45:53 +0200

    privacy policy page for gridlock

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Dartblock } from './dartblock/dartblock';
+import { GridLockPrivacyPolicyPage } from './views/projects/gridlock/privacy-policy/privacy-policy';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'dartblock',
     component: Dartblock,
+  },
+  {
+    path: 'gridlock/privacy-policy',
+    component: GridLockPrivacyPolicyPage,
   },
 ];
